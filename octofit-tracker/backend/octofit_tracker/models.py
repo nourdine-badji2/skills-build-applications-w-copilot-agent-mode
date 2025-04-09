89ab91025d8e98cf69f629bfcfd067be5533df22
nourdine-badji2 <129492101+nourdine-badji2@users.noreply.github.com>
2025-04-09 17:53:25 +0000

    Add database structure, update Django project files, and populate database

---
+++ octofit-tracker/backend/octofit_tracker/models.py
@@ -0,0 +1,24 @@
+from djongo import models
+
+class User(models.Model):
+    username = models.CharField(max_length=100)
+    email = models.EmailField()
+    password = models.CharField(max_length=100)
+
+class Team(models.Model):
+    name = models.CharField(max_length=100)
+    members = models.ManyToManyField('User')
+
+class Activity(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    type = models.CharField(max_length=100)
+    duration = models.IntegerField()
+    date = models.DateField()
+
+class Leaderboard(models.Model):
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    score = models.IntegerField()
+
+class Workout(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
